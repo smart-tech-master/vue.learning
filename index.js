c9cce1c4affc6eae5008f761ff482b7338ea3384
dzianis <dzianis@obv.io>
2022-03-02 07:07:09 -0600

    Update initialization

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ var app = new Vue({
         message: 'Hello Vue!'
     }
 });
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+})
